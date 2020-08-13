9fae9da6a42ccee5faf2fe32cb7185a404c93aff
Michael Boyle <michael.oliver.boyle@gmail.com>
2020-08-13 16:26:41 -0400

    Add decorator to enable returning q.arrays from jitted funcs

--- quaternionic/utilities.py
+++ quaternionic/utilities.py
@@ -3,3 +3,12 @@ import numba
 
 jit = functools.partial(numba.njit, cache=True)
 guvectorize = functools.partial(numba.guvectorize, nopython=True, cache=True)
+
+
+def type_self_return(f):
+    """Decorate jitted functions to return with type of first argument"""
+    @functools.wraps(f)
+    def wrapped(*args, **kwargs):
+        return_value = f(*args, **kwargs)
+        return type(args[0])(return_value)
+    return wrapped
