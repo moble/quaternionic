7deaa954b6ec9bff4de113d18e03ab0265074e83
Michael Boyle <michael.oliver.boyle@gmail.com>
2020-08-13 20:53:59 -0400

    Add another decorator for translating q.array<->ndarray

--- quaternionic/utilities.py
+++ quaternionic/utilities.py
@@ -12,3 +12,12 @@ def type_self_return(f):
         return_value = f(*args, **kwargs)
         return type(args[0])(return_value)
     return wrapped
+
+
+def ndarray_args(f):
+    """Decorate jitted functions to accept non-ndarrays"""
+    @functools.wraps(f)
+    def wrapped(*args, **kwargs):
+        args_ndarray = (arg if not hasattr(arg, 'ndarray') else arg.ndarray for arg in args)
+        return f(*args_ndarray, **kwargs)
+    return wrapped
