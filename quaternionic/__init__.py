1ee7d372d1c78294206067abfc6a8668921536b0
Michael Boyle <michael.oliver.boyle@gmail.com>
2020-08-14 15:59:00 -0400

    Consolidate version info into pyproject.toml

--- quaternionic/__init__.py
+++ quaternionic/__init__.py
@@ -2,12 +2,17 @@
 # See LICENSE file for details:
 # <https://github.com/moble/quaternionic/blob/master/LICENSE>
 
-__version__ = '0.0.1a'
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata
+
+__version__ = importlib_metadata.version(__name__)
 
 from .utilities import jit, guvectorize
 from . import algebra, distance, utilities
+
+algebra_ufuncs = type('algebra_gufuncs', (object,), dict())()
+utilities.guvectorize_module_functions(algebra, algebra_ufuncs)
+
 from .array import array
-
-
-algebra_gufuncs = type('algebra_gufuncs', (object,), dict())()
-utilities.guvectorize_module_functions(algebra, algebra_gufuncs)
