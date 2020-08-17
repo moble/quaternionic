8f065893b30578589866b968dbd4db095524e20e
Michael Boyle <michael.oliver.boyle@gmail.com>
2020-08-17 16:07:22 -0400

    Simplify logic in rotation metrics

--- quaternionic/distance.py
+++ quaternionic/distance.py
@@ -33,11 +33,12 @@ class rotation(object):
     @guvectorize([(float64[:], float64[:], float64[:])], '(n),(n)->()')
     def intrinsic(q1, q2, out):
         qtemp = np.empty(4)
-        out[0] = np.sqrt((q1[0]-q2[0])**2 + (q1[1]-q2[1])**2 + (q1[2]-q2[2])**2 + (q1[3]-q2[3])**2)
-        if out[0] > np.sqrt(2):
-            _divide(q1, -q2, qtemp)
-        else:
+        a = (q1[0]-q2[0])**2 + (q1[1]-q2[1])**2 + (q1[2]-q2[2])**2 + (q1[3]-q2[3])**2
+        b = (q1[0]+q2[0])**2 + (q1[1]+q2[1])**2 + (q1[2]+q2[2])**2 + (q1[3]+q2[3])**2
+        if b > a:
             _divide(q1, q2, qtemp)
+        else:
+            _divide(q1, -q2, qtemp)
         _log(qtemp, qtemp)
         _absolute(qtemp, out)
         out[0] *= 2
@@ -45,6 +46,9 @@ class rotation(object):
     @ndarray_args
     @guvectorize([(float64[:], float64[:], float64[:])], '(n),(n)->()')
     def chordal(q1, q2, out):
-        out[0] = np.sqrt((q1[0]-q2[0])**2 + (q1[1]-q2[1])**2 + (q1[2]-q2[2])**2 + (q1[3]-q2[3])**2)
-        if out[0] > np.sqrt(2):
-            out[0] = np.sqrt((q1[0]+q2[0])**2 + (q1[1]+q2[1])**2 + (q1[2]+q2[2])**2 + (q1[3]+q2[3])**2)
+        a = (q1[0]-q2[0])**2 + (q1[1]-q2[1])**2 + (q1[2]-q2[2])**2 + (q1[3]-q2[3])**2
+        b = (q1[0]+q2[0])**2 + (q1[1]+q2[1])**2 + (q1[2]+q2[2])**2 + (q1[3]+q2[3])**2
+        if b > a:
+            out[0] = np.sqrt(a)
+        else:
+            out[0] = np.sqrt(b)
