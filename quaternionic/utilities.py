be2fce38f2e7c026845d5e7aa9748172511bd1c5
Michael Boyle <michael.oliver.boyle@gmail.com>
2020-08-24 11:03:18 -0400

    Create FakeNumbaType for pypy

--- quaternionic/utilities.py
+++ quaternionic/utilities.py
@@ -194,12 +194,16 @@ def pyguvectorize_module_functions(module, obj):
 
 
 if platform.python_implementation().lower() == 'pypy':
-    float64 = type('nbfloat64', (object,), {
-        'name': 'float64', 'dtype': self, '__getitem__': lambda self, *args: self
-    })
-    boolean = type('nbboolean', (object,), {
-        'name': 'boolean', 'dtype': self, '__getitem__': lambda self, *args: self
-    })
+    class _FakeNumbaType(object):
+        def __init__(self, name):
+            self.name = name
+        @property
+        def dtype(self):
+            return self
+        def __getitem__(self, *args, **kwargs):
+            return self
+    float64 = _FakeNumbaType('float64')
+    boolean = _FakeNumbaType('boolean')
     jit = lambda f: f
     guvectorize = pyguvectorize
 else:
