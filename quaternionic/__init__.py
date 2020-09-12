3f4a22060ef58cad4d69c4f6070eca2da7165b0d
Michael Boyle <michael.oliver.boyle@gmail.com>
2020-09-12 15:47:06 -0400

    Add top-level constants one, x, y, z, i, j, k

--- quaternionic/__init__.py
+++ quaternionic/__init__.py
@@ -16,3 +16,18 @@ algebra_ufuncs = type('AlgebraUfuncs', (object,), dict())()
 utilities.guvectorize_module_functions(algebra, algebra_ufuncs)
 
 from .arrays import QuaternionicArray, array
+
+one = array(1, 0, 0, 0)
+one.flags.writeable = False
+
+x = array(0, 1, 0, 0)
+x.flags.writeable = False
+i = x
+
+y = array(0, 0, 1, 0)
+y.flags.writeable = False
+j = y
+
+z = array(0, 0, 0, 1)
+z.flags.writeable = False
+k = z
--- tests/test_quaternionic.py
+++ tests/test_quaternionic.py
@@ -2,3 +2,18 @@ import warnings
 import numpy as np
 import quaternionic
 import pytest
+
+
+def test_constants():
+    for const in ['one', 'x', 'y', 'z', 'i', 'j', 'k']:
+        assert hasattr(quaternionic, const)
+        c = getattr(quaternionic, const)
+        with pytest.raises(ValueError):
+            c *= 1.2
+    assert np.equal(quaternionic.one, quaternionic.array(1, 0, 0, 0))
+    assert np.equal(quaternionic.x, quaternionic.array(0, 1, 0, 0))
+    assert np.equal(quaternionic.i, quaternionic.array(0, 1, 0, 0))
+    assert np.equal(quaternionic.y, quaternionic.array(0, 0, 1, 0))
+    assert np.equal(quaternionic.j, quaternionic.array(0, 0, 1, 0))
+    assert np.equal(quaternionic.z, quaternionic.array(0, 0, 0, 1))
+    assert np.equal(quaternionic.k, quaternionic.array(0, 0, 0, 1))
